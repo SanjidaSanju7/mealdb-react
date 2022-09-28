24958da5a0923afd607e30511e17239581364d65
SanjidaSanju7 <sanjidaakter6789@gmail.com>
2022-09-28 15:36:54 +0600

    input field added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import './App.css';
 
 import Header from './components/Header/Header';
 import Meal from './components/Meal/Meal';
-import Meals from './components/Meals/Meals';
+
 
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Meals></Meals>
       <Meal></Meal>
     </div>
   );
--- src/components/Meal/Meal.js
+++ src/components/Meal/Meal.js
@@ -7,13 +7,14 @@ const Meal = () => {
 
     const [meals, setMeals] = useState([]);
     const [newMeals, setNewMeals] = useState([]);
+    const [searchText, setSearchText] = useState("");
 
 
     useEffect(() => {
-        fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`)
             .then(res => res.json())
             .then(data => setMeals(data.meals));
-    }, [])
+    }, [searchText])
 
     const cartHandler = (meal) => {
         const anotherMeals = [...newMeals, meal]
@@ -24,30 +25,34 @@ const Meal = () => {
         setNewMeals(anotherMeals);
     }
 
-
-
-
     return (
-        <div className='restaurant-container'>
-            <div className="meal-container">
-                {
-                    meals.map(meal => <SingleMeal
-                        meal={meal}
-                        key={meal.idMeal}
-                        cartHandler={cartHandler}
-                    ></SingleMeal>)
-                }
-            </div>
-            <div className='cart-container'>
-                <div className='cart'>
-                    <h3 className='summary'>Item Summary</h3>
-                    <ol>
-                        {
-                            newMeals.map(meal => <Cart
-                                meal={meal}
-                                key={meal.idMeal}></Cart>)
-                        }
-                    </ol>
+
+        <div className='container'>
+            <h1>Welcome To My Green Food</h1>
+            <input type="text" placeholder='Search By Name...' className='input-field'
+                onChange={(e) => setSearchText(e.target.value)} />
+
+            <div className='restaurant-container'>
+                <div className="meal-container">
+                    {
+                        meals.map(meal => <SingleMeal
+                            meal={meal}
+                            key={meal.idMeal}
+                            cartHandler={cartHandler}
+                        ></SingleMeal>)
+                    }
+                </div>
+                <div className='cart-container'>
+                    <div className='cart'>
+                        <h3 className='summary'>Item Summary</h3>
+                        <ol>
+                            {
+                                newMeals.map(meal => <Cart
+                                    meal={meal}
+                                    key={meal.idMeal}></Cart>)
+                            }
+                        </ol>
+                    </div>
                 </div>
             </div>
         </div>
